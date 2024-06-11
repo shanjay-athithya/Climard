b6d3d8dc6ed62791fc5630af6481f0f6e113aae8
shanjay-athithya <shanjayathithya2004@gmail.com>
2024-06-11 09:19:20 +0530

    c5

--- climard_server/webapp/urls.py
+++ climard_server/webapp/urls.py
@@ -3,7 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
-    path('led/', led, name='led'),
     path('register_esp32/', register_esp32, name='register_esp32'),
     path('get_esp32_ip/', get_esp32_ip, name='get_esp32_ip'),
     path('endpoint/', receive_data, name='receive_data'),
